d8a6500ab543628accb1cfe8ca5a364fc5bba826
ga111o <ga111o@proton.me>
2023-12-22 15:31:40 +0900

    this is streamlit

--- lecture/7/home.py
+++ lecture/7/home.py
@@ -1,3 +1,27 @@
-import streamlit
+import streamlit as st
+from datetime import datetime
+from langchain.prompts import PromptTemplate
 
-streamlit.title("ga11o!")
+today = datetime.today().strftime("%H:%M:%S")
+
+
+st.title("ga11o!")
+
+st.write(today)
+
+select_model = st.selectbox("select model!", ("llama2", "mistral"))
+
+st.write(select_model)
+
+if select_model == "llama2":
+    st.write("this is llama2:7b!")
+else:
+    st.write("this is mistral latest!")
+
+        
+    name = st.text_input("input your name!")
+
+    st.write(name)
+
+    value = st.slider("temp", min_value=0.1, max_value=0.9)
+    st.write(value)
