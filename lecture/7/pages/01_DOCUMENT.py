b46cbf6717644f3806abb77a5a3cb016d244de85
ga111o <ga111o@proton.me>
2023-12-22 15:53:40 +0900

    chat message test

--- lecture/7/pages/01_DOCUMENT.py
+++ lecture/7/pages/01_DOCUMENT.py
@@ -1,3 +1,27 @@
 import streamlit as st
+import time
+
+st.set_page_config(
+    page_title= "DOCUMENT",
+    page_icon="📄",
+)
 
 st.title("ga111o! DOCUMENT")
+
+with st.chat_message("human"):
+    st.write("ga11o!")
+
+with st.chat_message("ai"):
+    st.write("how are u?")
+
+st.chat_input("SEND A MESSAGE")
+
+with st.status("embeding file..", expanded=True) as status:
+    time.sleep(2)
+    st.write("getting the file")
+    time.sleep(3)
+    st.write("embeding the file")
+    time.sleep(3)
+    st.write("caching the file")
+    status.update(label="ERROR", state="error")
+
