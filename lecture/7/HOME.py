2741eb1dd2a149ee137af07b5f066beb6606a2d9
ga111o <ga111o@proton.me>
2023-12-22 15:44:56 +0900

    create some pages

---
+++ lecture/7/HOME.py
@@ -0,0 +1,13 @@
+# streamlit run HOME.py
+
+
+import streamlit as st
+from datetime import datetime
+from langchain.prompts import PromptTemplate
+
+st.set_page_config(
+    page_title= "ga111o!",
+    page_icon="🤖",
+)
+
+st.title("ga111o! HOME")
