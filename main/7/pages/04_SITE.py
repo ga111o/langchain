e046a34d262de95cf88b6dc03a706b4596efc11f
ga111o <ga111o@proton.me>
2024-01-03 14:57:18 +0900

    import optimazing

--- main/7/pages/04_SITE.py
+++ main/7/pages/04_SITE.py
@@ -1,12 +1,8 @@
 import streamlit as st
 from langchain.chat_models import ChatOllama
-from langchain.callbacks.base import BaseCallbackHandler
-from langchain.document_loaders import UnstructuredFileLoader
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OllamaEmbeddings, CacheBackedEmbeddings
+from langchain.embeddings import OllamaEmbeddings
 from langchain.vectorstores import FAISS
 from langchain.schema.runnable import RunnableLambda, RunnablePassthrough
-from langchain.storage import LocalFileStore
 from langchain.prompts import ChatPromptTemplate
 from langchain.document_loaders import SitemapLoader
 from langchain.document_transformers import Html2TextTransformer
